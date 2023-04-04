1c6d8d4f0e41fd8335a9ad3a75281b56ab10a6d7
Moizes J. Sousa <milyth@proton.me>
2023-04-04 18:09:43 -0300

    Polish system. As anyways

--- User/Editor/doom/config.el
+++ User/Editor/doom/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "JetBrains Mono" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 13))
+      doom-variable-pitch-font (font-spec :family "Fantasque Sans Mono" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-spacegrey)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -74,3 +74,11 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(defun kill-other-buffers ()
+      "Kill all other buffers."
+      (interactive)
+      (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
+(setq fancy-splash-image "~/pics/doom-emacs-color.png")
+(setq lsp-ui-sideline-enable t)
