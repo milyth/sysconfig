565b0f20f0bbc4f8ece3554f6466cd13d2f53195
Moizes J. Sousa <milyth@duck.com>
2023-06-25 23:58:13 -0300

    Should I talk to my friend?

--- User/Shell/Fish/conf.d/env.fish
+++ User/Shell/Fish/conf.d/env.fish
@@ -2,6 +2,6 @@ set -gx GTK_USE_PORTAL 1
 set -gx CC clang
 set -gx CXX clang++
 set -gx GPG_TTY (tty)
-
+set -gx DESKTOP_SESSION Herbstluft
 set -gx PATH $PATH $HOME/.local/bin
 set -gx PATH $PATH $HOME/.cargo/bin
